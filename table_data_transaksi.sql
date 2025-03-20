22dd81fa269b3a70fe0946d9cca8c88df813a376
Melody Priscilla <125964713+meeeeeeeel@users.noreply.github.com>
2025-03-20 15:08:22 +0700

    table_data_transaksi.sql

--- table_data_transaksi.sql
+++ table_data_transaksi.sql
@@ -10,16 +10,17 @@ CASE
   ELSE 0.3
 END AS persentase_gross_laba,
 p.price * (1 - t.discount_percentage) AS nett_sales,
-CASE 
-  WHEN p.price <= 50000 THEN p.price * (1 - t.discount_percentage) * 0.1
-  WHEN p.price <= 100000 THEN p.price * (1 - t.discount_percentage) * 0.15
-  WHEN p.price <= 300000 THEN p.price * (1 - t.discount_percentage) * 0.2
-  WHEN p.price <= 500000 THEN p.price * (1 - t.discount_percentage) * 0.25
-  ELSE p.price * (1 - t.discount_percentage) * 0.3
-END AS nett_profit,
+p.price * (1 - t.discount_percentage) *
+  CASE 
+    WHEN p.price <= 50000 THEN 0.1
+    WHEN p.price <= 100000 THEN 0.15
+    WHEN p.price <= 300000 THEN 0.2
+    WHEN p.price <= 500000 THEN 0.25
+    ELSE 0.3
+  END AS nett_profit,
 t.rating AS rating_transaksi
 FROM `kimia_farma.kf_final_transaction` t
 JOIN `kimia_farma.kf_kantor_cabang` b 
-ON t.branch_id = b.branch_id
+  ON t.branch_id = b.branch_id
 JOIN `kimia_farma.kf_product` p
-ON t.product_id = p.product_id
+  ON t.product_id = p.product_id
